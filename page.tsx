36c54e495237d2f82067cdebc23855ac153fbe5d
bilal-raza12 <160725888+bilal-raza12@users.noreply.github.com>
2024-09-07 02:59:09 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+
+import  Calculator  from "@/components/Calculator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+     
+      <Calculator />
+    </div>
+  );
+}
